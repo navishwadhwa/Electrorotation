9124c14cdc4bc8bee04edca6fc410416cd3fe601
Siyu He <copper.helium45@gmail.com>
2017-04-19 22:42:35 +0800

    Update and rename ps_plot to ps_plot.m

--- ps_plot.m
+++ ps_plot.m
@@ -1,10 +1,10 @@
-function y= ps_plot(peak,real_peak,pottsL2,step_modified,freq_int,Inter,real_time,label_step,minfre,maxfre,file_name,rec)
+function y= ps_plot(spec_sampling,periods,peak,real_peak,pottsL2,step_modified,freq_int,Inter,real_time,label_step,minfre,maxfre,file_name,rec)
 
+        Inter=Inter./spec_sampling;
         pottsL3 = pottsL2;
         z=10*ones(1,length(peak));
         hold on;
         timescale = linspace(1,length(peak),length(peak));
-        insert=[1:length(timescale)/length(real_time):length(timescale)];
         plot3(timescale,real_peak,z,'.','color','k','MarkerSize',8);
         plot3(timescale,pottsL3,z,'g--.', 'MarkerSize', 10);
         plot3(timescale,step_modified,z,'r--.', 'MarkerSize', 10);
@@ -16,18 +16,18 @@ function y= ps_plot(peak,real_peak,pottsL2,step_modified,freq_int,Inter,real_tim
 hold on; 
 if rec==0
 for i=1:length(Inter)
-    plot3(xline+insert(i),yline,zline,'--w');
+    plot3(xline+Inter(i),yline,zline,'--w');
 end
 end
   
   
- 
+
   axis tight;       
 %set x labels
   hold on;
   set(gca,'xtick',[]);
-  set(gca,'xtick',insert(1:label_step:end));
-  set(gca,'xticklabel',{real_time(1:label_step:end)});  
+  set(gca,'xtick',Inter(1:label_step:end));
+  set(gca,'xticklabel',{floor(periods(1:label_step:end,1))}); 
   legend('Intensity of WT','Peak','Step','Step modified','Location','best');
   
 end
